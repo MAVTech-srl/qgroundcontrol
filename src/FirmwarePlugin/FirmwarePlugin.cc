34dc1c976092b0bdffb00908a35c8e8626d2cff2
Fabio Schick <58027418+mrschick@users.noreply.github.com>
2026-06-11 22:15:08 +0200

    Disabilita i messaggi di errore firmware

--- src/FirmwarePlugin/FirmwarePlugin.cc
+++ src/FirmwarePlugin/FirmwarePlugin.cc
@@ -356,10 +356,10 @@ void FirmwarePlugin::_versionFileDownloadFinished(const QString &remoteFile, con
 
     // Check if lower version than stable or same version but different type
     if ((currType == FIRMWARE_VERSION_TYPE_OFFICIAL) && (vehicle->versionCompare(version) < 0)) {
-        const QString currentVersionNumber = QStringLiteral("%1.%2.%3").arg(vehicle->firmwareMajorVersion())
+        /*const QString currentVersionNumber = QStringLiteral("%1.%2.%3").arg(vehicle->firmwareMajorVersion())
                                                                        .arg(vehicle->firmwareMinorVersion())
                                                                        .arg(vehicle->firmwarePatchVersion());
-        QGC::showAppMessage(tr("Vehicle is not running latest stable firmware! Running %1, latest stable is %2.").arg(currentVersionNumber, version));
+        QGC::showAppMessage(tr("Vehicle is not running latest stable firmware! Running %1, latest stable is %2.").arg(currentVersionNumber, version));*/
     }
 }
 
