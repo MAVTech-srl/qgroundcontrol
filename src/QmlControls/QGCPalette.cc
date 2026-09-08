51be58b27753e1d85daaaf92c1ee08e24db95335
Fabio Schick <58027418+mrschick@users.noreply.github.com>
2026-09-08 14:30:54 +0200

    Logo e gradiente blu MAVTech
    Sostituito il logo QGC con quello MAVTech e il gradiente viola QGC della barra superiore (quando non è connesso alcun drone) con un gradiente blu MAVTech

--- src/QmlControls/QGCPalette.cc
+++ src/QmlControls/QGCPalette.cc
@@ -76,7 +76,7 @@ void QGCPalette::_buildMap()
 
     // Colors not affecting by theming
     //                                                      Disabled     Enabled
-    DECLARE_QGC_NONTHEMED_COLOR(brandingPurple,             "#4A2C6D", "#4A2C6D")
+    DECLARE_QGC_NONTHEMED_COLOR(brandingPurple,             "#075895", "#075895")
     DECLARE_QGC_NONTHEMED_COLOR(brandingBlue,               "#48D6FF", "#6045c5")
     DECLARE_QGC_NONTHEMED_COLOR(toolStripFGColor,           "#707070", "#ffffff")
     DECLARE_QGC_NONTHEMED_COLOR(photoCaptureButtonColor,    "#707070", "#ffffff")
